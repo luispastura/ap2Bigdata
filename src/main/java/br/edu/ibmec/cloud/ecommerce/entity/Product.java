121152f95450e57a4e7392ada0aca5cf4f42e047
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-10-10 11:44:46 -0300

    first commit

---
+++ src/main/java/br/edu/ibmec/cloud/ecommerce/entity/Product.java
@@ -0,0 +1,23 @@
+package br.edu.ibmec.cloud.ecommerce.entity;
+
+import org.springframework.data.annotation.Id;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+
+import lombok.Data;
+
+@Data
+@Container(containerName = "products")
+public class Product {
+
+    @Id    
+    private String productId;
+    
+    private String productName;
+    
+    @PartitionKey
+    private String productCategory;
+    
+    private double price;
+}
